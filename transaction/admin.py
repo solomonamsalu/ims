25a572e8150f64f0ab5077864a81f3dfcae3676b
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-20 16:17:47 +0300

    Add purchase order to admin page

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import PurchaseOrder
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    
+    list_display = [field.name for field in PurchaseOrder._meta.fields]
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
