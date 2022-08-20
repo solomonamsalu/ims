a1adf1bdeeba5a709fa3b0897da9d3e26d0f971f
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-20 21:12:54 +0300

    delete all apps

--- transaction/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-from .models import PurchaseOrder
-
-class PurchaseOrderAdmin(admin.ModelAdmin):
-    
-    list_display = [field.name for field in PurchaseOrder._meta.fields]
-admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
