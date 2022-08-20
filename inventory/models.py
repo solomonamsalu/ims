94d930c39f4d42374e3f14ddae8b1363d9921e12
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-20 15:32:24 +0300

    add reorder_level field to stock model

--- inventory/models.py
+++ inventory/models.py
@@ -4,7 +4,10 @@ class Stock(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, unique=True)
     quantity = models.IntegerField(default=1)
+    reorder_level=models.IntegerField()
+
     is_deleted = models.BooleanField(default=False)
+
 
     def __str__(self):
 	    return self.name
