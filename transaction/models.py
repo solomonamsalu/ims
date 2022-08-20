07aae7f4ee835fe2d1839bac38d074a827a0e06b
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-20 16:08:44 +0300

    add PurchaseOrder model into transactions app

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from inventory.models import Stock
+
+class PurchaseOrder(models.Model):
+
+    stock=models.ForeignKey(Stock,on_delete=models.CASCADE)
+    quantity=models.IntegerField()
+    order_date=models.DateTimeField(auto_now_add=True)
+    vendor=models.CharField(max_length=100)
+    status=models.CharField(max_length=100)
