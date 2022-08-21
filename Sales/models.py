e79f8427cae5418e4638cc7c043416533559c6ea
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-21 11:24:38 +0300

    create customer class

--- Sales/models.py
+++ Sales/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=200,unique=True)
+    phone=models.CharField(max_length=20,unique=True)
+    address=models.CharField(max_length=200)
+    city=models.CharField(max_length=200)
+    country=models.CharField(max_length=200)
+    postal=models.CharField(max_length==200)
+    
