38a6c613e10e17663d0eeb15fc11d1334e7fbf03
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-22 11:35:52 +0300

    DEBUG: add import supplier

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,6 +1,6 @@
 from tkinter import CASCADE
 from django.db import models
-
+from Purchase import Supplier
 class Item(models.Model):
     name=models.CharField(max_length=100)
     SKU_price=models.IntegerField()
@@ -9,4 +9,4 @@ class Item(models.Model):
     on_hand_stock=models.IntegerField()
     reorder_point=models.IntegerField()
     Preferred_supplier=models.ForeignKey(Supplier,on_delete=CASCADE)
-
+    
