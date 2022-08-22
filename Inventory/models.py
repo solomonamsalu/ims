20729f5a14c8aba40be7c630c0f0a4a8e2320829
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-22 11:44:59 +0300

    Fix item model

--- Inventory/models.py
+++ Inventory/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from Purchase import Supplier
 class Item(models.Model):
     name=models.CharField(max_length=100)
-    SKU_price=models.IntegerField()
-    seling_price=models.DecimalField()
-    cost_price=models.DecimalField()
+    SKU_number=models.IntegerField()
+    seling_price=models.FloatField()
+    cost_price=models.FloatField()
     on_hand_stock=models.IntegerField()
     reorder_point=models.IntegerField()
     Preferred_supplier=models.ForeignKey(Supplier,on_delete=CASCADE)
