02e262644a67b2023487bc4bf0acd2f95b4bcda3
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-08-22 21:38:28 +0300

    add bootstrap5

--- ims/settings.py
+++ ims/settings.py
@@ -119,3 +119,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
