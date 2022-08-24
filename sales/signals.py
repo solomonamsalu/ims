4e595f5bb3f2bf781c8388cd73925b1ac9f5284e
MarkosMuche <markosmuche2018@gmail.com>
2022-08-24 08:58:33 +0300

    signal for subtracting item.on_hand_stock done

--- sales/signals.py
+++ sales/signals.py
@@ -2,22 +2,21 @@
 
 
 
-# import os
-# from django.conf import settings
-# from django.core.mail import send_mail
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+import os
+from django.conf import settings
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# from sales.models import SalesOrder
-# from inventory.models import Item
+from sales.models import SalesOrder
+from inventory.models import Item
 
-# @receiver(post_save, sender=SalesOrder)
-# def signoff(sender, instance, **kwargs):
-#     ''' Create sign off and send email to those who have to sign.'''
-#     sales_order = instance
-    
-#     items = sales_order.items.all()
-
-#     for item in items:
-
-#         item.on_hand_stock -= 
+@receiver(post_save, sender=SalesOrder)
+def signoff(sender, instance, **kwargs):
+    ''' Create sign off and send email to those who have to sign.'''
+    sales_order = instance
+    item = sales_order.item
+    item.on_hand_stock -= sales_order.quantity
+    post_save.disconnect(signoff, sender=sender)
+    item.save()
+    post_save.connect(signoff, sender=sender)
