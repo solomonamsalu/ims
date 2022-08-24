c503c2229f6f6d04711997eb87689b37c49dc158
MarkosMuche <markosmuche2018@gmail.com>
2022-08-24 08:59:47 +0300

    signals debug

--- sales/signals.py
+++ sales/signals.py
@@ -1,8 +1,5 @@
 
 
-
-
-import os
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
@@ -12,11 +9,11 @@ from sales.models import SalesOrder
 from inventory.models import Item
 
 @receiver(post_save, sender=SalesOrder)
-def signoff(sender, instance, **kwargs):
+def order_done(sender, instance, **kwargs):
     ''' Create sign off and send email to those who have to sign.'''
     sales_order = instance
     item = sales_order.item
     item.on_hand_stock -= sales_order.quantity
-    post_save.disconnect(signoff, sender=sender)
+    post_save.disconnect(order_done, sender=sender)
     item.save()
-    post_save.connect(signoff, sender=sender)
+    post_save.connect(order_done, sender=sender)
