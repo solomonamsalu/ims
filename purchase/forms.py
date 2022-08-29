d03c75fa68ba27e65949d8b5973f12aee9cc27b8
MarkosMuche <markosmuche2018@gmail.com>
2022-08-30 01:46:59 +0300

    only items in the list appear.

--- purchase/forms.py
+++ purchase/forms.py
@@ -3,10 +3,27 @@ from django import forms
 from inventory.models import Item
 
 from purchase.models import PurchaseOrder
+from core.models import User
+
 
 class AddPurchaseOrderForm(forms.ModelForm):
+  
 
-    # item = forms.ModelChoiceField(queryset = Item.objects.filter(store__))
+    def __init__(self,  *args,  **kwargs):
+        
+        try:
+            user = kwargs.pop('user') # Important to do this
+        except:
+            pass
+        super(AddPurchaseOrderForm, self).__init__(*args, **kwargs)
+        user = User.objects.all()[0]
+        if user.company_owner:
+            self.fields['item'].queryset = Item.objects.filter(store_company=user.company)
+            print()
+        else:
+            self.fields['item'].queryset = Item.objects.filter(store=user.store)
+            print()
+
     class Meta:
         model=PurchaseOrder
         fields = ['supplier', 'deliver_to', 'purchase_order_number',  'item', 'quantity', 'rate']
